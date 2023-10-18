dd32ebf3f1eb34e8e75d1408658f5b0e6a010a6a
PiresPires0 <piresgabrielpxx@gmail.com>
2023-10-17 23:17:57 -0300

    Primeiro Commit

---
+++ app/src/main/java/devandroid/gabriel/tentativa_curso2/MainActivity.java
@@ -0,0 +1,14 @@
+package devandroid.gabriel.tentativa_curso2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
