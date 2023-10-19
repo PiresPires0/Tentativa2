99799d34e88455e16ddc3933704449399b2bae5b
PiresPires0 <piresgabrielpxx@gmail.com>
2023-10-19 15:04:17 -0300

    Quinto commit:Criação de classes e com declaração e instaciamento

---
+++ app/src/main/java/devandroid/gabriel/tentativa_curso2/Model/Pessoa.java
@@ -0,0 +1,9 @@
+package devandroid.gabriel.tentativa_curso2.Model;
+
+public class Pessoa {
+
+    //CONSTRUCTOR
+    public Pessoa(){
+
+    }
+}
--- app/src/main/java/devandroid/gabriel/tentativa_curso2/View/MainActivity.java
+++ app/src/main/java/devandroid/gabriel/tentativa_curso2/View/MainActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import devandroid.gabriel.tentativa_curso2.Model.Pessoa;
 import devandroid.gabriel.tentativa_curso2.R;
 
 public class MainActivity extends AppCompatActivity {
+    Pessoa pessoa; //Declaraçao de objeto
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pessoa = new Pessoa(); //Criaçao do objeto(Instaciando)
     }
 }
