8612d12f3285435d7604be5eaa609548c0db018b
PiresPires0 <piresgabrielpxx@gmail.com>
2023-11-08 06:18:54 -0300

    23-Commit ->Criação de uma segunda pagina

---
+++ app/src/main/java/devandroid/gabriel/tentativa_curso2/View/SplashActivity.java
@@ -0,0 +1,16 @@
+package devandroid.gabriel.tentativa_curso2.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import devandroid.gabriel.tentativa_curso2.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(androidx.appcompat.R.layout.activity_splash);
+    }
+}
