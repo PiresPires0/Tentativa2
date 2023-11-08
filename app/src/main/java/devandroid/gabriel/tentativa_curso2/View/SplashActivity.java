d3cec52fca24bfc43b9d23b9beedf0dfd499793f
PiresPires0 <piresgabrielpxx@gmail.com>
2023-11-08 08:41:32 -0300

    24-Commit ->Finalização do projeto/parte2

--- app/src/main/java/devandroid/gabriel/tentativa_curso2/View/SplashActivity.java
+++ app/src/main/java/devandroid/gabriel/tentativa_curso2/View/SplashActivity.java
@@ -2,12 +2,14 @@ package devandroid.gabriel.tentativa_curso2.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
 import devandroid.gabriel.tentativa_curso2.R;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
 public static final int TIME_SPLASH = 3000;
 
@@ -20,13 +22,10 @@ public static final int TIME_SPLASH = 3000;
     }
 
     private void comutar_tela_splash() {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent Tela_Principal = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(Tela_Principal);
-                finish();
-            }
+        new Handler().postDelayed(() -> {
+            Intent Tela_Principal = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(Tela_Principal);
+            finish();
         },TIME_SPLASH);
     }
 
